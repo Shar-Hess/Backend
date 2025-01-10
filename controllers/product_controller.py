7f81aa76fad57c451e8d5638a7de932818827056
Sharlane Hess <sharlanehess@Sharlanes-MacBook-Pro.local>
2025-01-09 17:50:50 -0700

    crud working

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -1,25 +1,6 @@
 from flask import jsonify, request
 from data import product_records
 
-def update_product(product_id):
-    post_data = request.form if request.form else request.json
-
-    product = {}
-    product['product_id'] = post_data.get('product_id')
-
-    for record in product_records:
-        if record['product_id'] == int(product_id):
-            product = record
-
-
-    product['name'] = post_data.get('name', product['name'])
-    product['description'] = post_data.get('description', product['description'])
-    product['price'] = post_data.get('price', product['price'])
-
-    product_records.append(product)
-    return jsonify({"message": f"Product {product['name']} has been updated."}), 200
-
-
 def create_product():
     post_data = request.form if request.form else request.json
     product = {}
@@ -30,10 +11,50 @@ def create_product():
     product['active'] = post_data['active']
     product_records.append(product)
 
-    return jsonify({"message": f"Product {product['name']} has been added."}), 201
+    return jsonify({"message": f"product {product['name']} has been added."}), 201
 
-def get_product_by_id(product_id):
+def read_products():
+    return jsonify({"message": f'here are all products {product_records}'}), 200
+
+def read_active_products():
+    active_products = [product for product in product_records if product["active"]]
+    return jsonify({"message": f'here are all active products {active_products}'}), 200
+
+def read_product_by_id(product_id):
     for product in product_records:
-        if product['product_id'] == int(product_id):
+        if product["product_id"] == product_id:
             return jsonify(product), 200
-    return jsonify({"message": f'Product with id {product_id} not found.'}), 404
+    return jsonify({"error": "Product not found"}), 404
+
+
+def update_product_by_id(product_id):
+    post_data = request.form if request.form else request.json
+
+    for product in product_records:
+        print(product)
+        if product['product_id'] == product_id:
+            product["name"] = post_data.get("name", product["name"])
+            product["description"] = post_data.get("description", product["description"])
+            product["price"] = post_data.get("price", product["price"])
+            product["active"] = post_data.get("active", product["active"])  
+
+            return jsonify({"message": f"Product {product['name']} has been updated."}), 200
+    
+    return jsonify({"error": "Product not found"}), 404
+
+
+def toggle_product_activity(product_id):
+    for product in product_records:
+        if product["product_id"] == product_id:
+            product["active"] = not product["active"]
+            return jsonify({"message": "Product activity toggled", "product": product}), 200
+    
+    return jsonify({"error": "Product not found"}), 404
+
+def delete_product_by_id(product_id):
+    for product in product_records:
+        if product["product_id"] == int(product_id):
+            product_records.remove(product)
+
+            return jsonify({"message": f"Product with id {product_id} has been deleted."}), 200
+    return jsonify({"error": "Product not found"}), 404
--- routes/product_routes.py
+++ routes/product_routes.py
@@ -3,14 +3,31 @@ import controllers
 
 product = Blueprint('product', __name__)
 
-@product.route('/product/<product_id>', methods=['GET'])
-def get_product_by_id(product_id):
-    return controllers.get_product_by_id(product_id)
-
 @product.route('/product', methods=['POST'])
 def create_product():
-    return controllers.create_product(product_id)
+    return controllers.create_product()
+
+@product.route('/products', methods=['GET'])
+def read_products():
+    return controllers.read_products()
+
+@product.route('/products/active', methods=['GET'])
+def read_active_products():
+    return controllers.read_active_products()
+
+@product.route('/product/<product_id>', methods=['GET'])
+def read_product_by_id(product_id):
+    return controllers.read_product_by_id(product_id)
+
 
 @product.route('/product/<product_id>', methods=['PUT'])
-def update_product(product_id):
-    return controllers.update_product(product_id)
+def update_product_by_id(product_id):
+    return controllers.update_product_by_id(product_id)
+
+@product.route('/product/activity/<product_id>', methods=['PATCH'])
+def toggle_product_activity(product_id):
+    return controllers.toggle_product_activity(product_id)
+
+@product.route('/product/delete/<product_id>', methods=['DELETE'])
+def delete_product_by_id(product_id):
+    return controllers.delete_product_by_id(product_id)
